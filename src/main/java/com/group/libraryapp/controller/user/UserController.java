016ad90aa80cf90c18e41506e72683ebbe51453a
soo1e <gameswuon@gmail.com>
2024-02-20 01:40:19 +0900

    0220 sec2_1

---
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -0,0 +1,44 @@
+package com.group.libraryapp.controller.user;
+
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+  private final JdbcTemplate jdbcTemplate;
+
+  public UserController(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @PostMapping("/user") // POST /user
+  public void saveUser(@RequestBody UserCreateRequest request) {
+    String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+    jdbcTemplate.update(sql, request.getName(), request.getAge());
+  }
+
+  @GetMapping("/user")
+  public List<UserResponse> getUsers() {
+    String sql = "SELECT * FROM user";
+    return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+      @Override
+      public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+        long id = rs.getLong("id");
+        String name = rs.getString("name");
+        int age = rs.getInt("age");
+        return new UserResponse(id, name, age);
+      }
+    });
+  }
+}
+
